c317ce807580cdb7b22297783a7e6c1c1c3c398a
DubanMorales <duban.morales@woodbury.edu>
2023-03-15 16:59:25 -0700

    updates

---
+++ Studio4BFIN/test1.js
@@ -0,0 +1,15 @@
+let username = prompt("enter username");
+
+if(username == "Admin"){
+    let pass = prompt("Enter password");
+
+
+        if(pass == "12345"){
+            alert("welcome!");
+        } else {
+            alert("wrong password");
+        
+        }
+} else {
+        alert("Try Again");
+}
---
+++ javascript/Notestest.js
@@ -0,0 +1,67 @@
+//let inClass = confirm("can you click ok?");
+
+//if(inClass == true){
+//    alert("Good job!");
+
+
+//}
+
+//if(inClass == false){
+
+//    alert("Thats too bad");
+
+//}
+// 
+
+let year = promt("What year were you born?");
+
+if (year < 2000) {
+    alert("you're old!");
+} else if (year > 2004) {
+    alert("are you old enough to be in college?")
+} else {
+    alert("sounds about right ")
+}
+
+
+//
+
+
+let inClass = prompt("what does html stand for?");
+
+if(answer == "Hyper Text Markup Language"){
+    alert("Correct!");
+} else{
+    alert("try again")
+}
+
+//
+
+let inClass = prompt("enter username");
+
+if(answer == "Admin"){
+    alert("Enter password");
+} else{
+    alert("try again")
+}
+        if(answer == "12345"){
+            alert("welcome!")
+        } else {
+            alert("wrong password")
+        }
+
+        let username = prompt("enter username");
+
+        if(username == "Admin"){
+            let pass = prompt("Enter password");
+        
+        
+                if(pass == "12345"){
+                    alert("welcome!");
+                } else {
+                    alert("wrong password");
+                
+                }
+        } else {
+                alert("Try Again");
+        }
